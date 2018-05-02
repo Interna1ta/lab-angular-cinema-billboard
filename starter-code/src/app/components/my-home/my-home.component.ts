32195498d005bfe9238b61d632796252ded34cd4
Pere Serra Bosch <pereserrabosch@MacBook-Pro-de-Pere.local>
2018-05-02 22:00:54 +0200

    clean code

--- starter-code/src/app/components/my-home/my-home.component.ts
+++ starter-code/src/app/components/my-home/my-home.component.ts
@@ -17,19 +17,6 @@ export class MyHomeComponent implements OnInit {
   ngOnInit() {
     this.movies = this.moviesService.getMovies();
     console.log(this.movies);
-    // this.movies.push(this.newContact)
-
-    // for (let i=0; i<this.movies.length; i++) {
-    //   this.movies[i];
-    // }
-
   }
 
 }
-
-
-
-// Load the movies from the Cinema service, with the getMovies() method we've previously defined.
-// Display the movies on the / home page.
-// Create a link for each movie that directs the user to / movie /: id.
-// Style the page.
